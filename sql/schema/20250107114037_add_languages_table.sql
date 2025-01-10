ad2681f26fa24e1105c6d529dc8beaf49b165a0d
TKyleB <kyle.beasley22@gmail.com>
2025-01-09 20:58:33 -0500

    adjust caps on seeded languages

--- sql/schema/20250107114037_add_languages_table.sql
+++ sql/schema/20250107114037_add_languages_table.sql
@@ -6,13 +6,13 @@ CREATE TABLE languages(
 );
 
 INSERT INTO languages(id, name)
-VALUES(gen_random_uuid(), 'Python');
+VALUES(gen_random_uuid(), 'python');
 
 INSERT INTO languages(id, name)
-VALUES(gen_random_uuid(), 'Javascript');
+VALUES(gen_random_uuid(), 'javascript');
 
 INSERT INTO languages(id, name)
-VALUES(gen_random_uuid(), 'Go');
+VALUES(gen_random_uuid(), 'go');
 -- +goose StatementEnd
 
 -- +goose Down
