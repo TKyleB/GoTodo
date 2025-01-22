bf964b355d1e4a213da5f13ce997735923ab1989
TKyleB <kyle.beasley22@gmail.com>
2025-01-21 21:40:47 -0500

    added revoke refresh token

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -8,3 +8,10 @@ SELECT * FROM refresh_tokens
 WHERE token = $1
 AND expires_at > NOW()
 AND revoked_at IS NULL;
+
+-- name: RevokeRefreshToken :one
+UPDATE refresh_tokens
+SET revoked_at = NOW(),
+    updated_at = NOW()
+WHERE token = $1
+RETURNING *;
